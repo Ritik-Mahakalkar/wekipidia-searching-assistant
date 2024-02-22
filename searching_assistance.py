c6e2ccc89501aa7f9f6eb65d401a254b0823b9d6
Ritik Mahakalkar <153834897+Ritik-Mahakalkar@users.noreply.github.com>
2024-02-22 14:46:20 +0530

    Add files via upload

---
+++ searching_assistance.py
@@ -0,0 +1,20 @@
+import pyttsx3
+import wikipedia
+from datetime import datetime
+from datetime import date,time
+
+
+today=date.today()
+td=today.strftime("%d/%m/%Y")
+
+
+local_time = datetime.now()
+current_time = local_time.strftime("%H:%M:%S")
+
+voices=pyttsx3.init()
+Input=input(F"\n Search Here  at {td} : ")
+result=wikipedia.summary(Input,sentences=2)#change the the no of senteces which want to be written  as an output chage value of sentence
+print("\nThe result at ",current_time,"of given input which is '",Input," 'are :\n")
+print(result)
+voices.say(result)
+voices.runAndWait()
